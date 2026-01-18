d819eb0545a870408780f990a2d872a45374b137
Keegan Erdis <erdisk@sheridancollege.ca>
2026-01-17 22:03:30 -0500

    Adjust Mic mute logic

--- frontend/src/lib/components/SessionControls.svelte
+++ frontend/src/lib/components/SessionControls.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-	let {
-		micActive = $bindable(true),
-		isConnected = false,
-		onEndSession
-	}: {
+	interface Props {
 		micActive?: boolean;
 		isConnected?: boolean;
 		onEndSession: () => void;
-	} = $props();
+	}
+
+	let {
+		micActive = $bindable(true),
+		isConnected = false,
+		onEndSession,
+	}: Props = $props();
 
 	function toggleMic() {
 		micActive = !micActive;
@@ -37,7 +39,9 @@
 				stroke-linecap="round"
 				stroke-linejoin="round"
 			>
-				<path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z" />
+				<path
+					d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z"
+				/>
 				<path d="M19 10v2a7 7 0 0 1-14 0v-2" />
 				<line x1="12" x2="12" y1="19" y2="22" />
 			</svg>
